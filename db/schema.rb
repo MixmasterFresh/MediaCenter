4861671ed23c44190234385d2bfed920f8322d5e
Austin Horning <austinseven@outlook.com>
2015-07-25 17:11:51 -0400

    File upload

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150725165517) do
+ActiveRecord::Schema.define(version: 20150725174134) do
 
   create_table "players", force: true do |t|
     t.datetime "created_at"
@@ -38,6 +38,10 @@ ActiveRecord::Schema.define(version: 20150725165517) do
     t.text     "artist"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "file_file_name"
+    t.string   "file_content_type"
+    t.integer  "file_file_size"
+    t.datetime "file_updated_at"
   end
 
 end
