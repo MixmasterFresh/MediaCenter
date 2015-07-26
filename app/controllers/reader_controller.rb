aa3c0c74a465305b54620358e5bca40fb4230ebc
brownba <brownba1@rose-hulman.edu>
2015-07-25 21:30:08 -0400

    updating html

--- app/controllers/reader_controller.rb
+++ app/controllers/reader_controller.rb
@@ -1,5 +1,17 @@
 class ReaderController < ApplicationController
 
   def index
+    @song = Song.new
+  end
+
+  def upload_song
+    @song = Song.create(song_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:title, :file)
   end
 end
